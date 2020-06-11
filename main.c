228e227997ba71ded1c11cbf569f1e855c558af0
burmistrok <ok.burmistr@gmail.com>
2020-06-11 14:28:27 +0300

    Execise 1-3

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+/*print Fahrenheit-Celsius table
+	for fahr = 0, 20, ..., 300; floating-point version */
 int main (void)
 {
-	printf("Hello, World!!!\n");
+	float fahr, celsius;
+	float lower, upper, step;
+
+	lower = 0; 		//lower limit of temperatuire scale
+	upper = 300; 	//upper limit
+	step = 20;		//step size
+
+	fahr = lower;
+
+	//print header
+	printf("Fahrenheit\tCelsius\n");
+
+	while (fahr <= upper)
+	{
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+		printf ("%3.0f\t\t%6.1f\n", fahr, celsius);
+		fahr += step;
+	}
+
 	return 0;
 }
